1c8a0c0687e76f93dc4d8ec99981338b8215f039
Jason <Jasonon@126.com>
2017-03-27 14:52:51 +0800

    V170327-2

--- Python_Advanced/01Iterator.py
+++ Python_Advanced/01Iterator.py
@@ -9,9 +9,10 @@ try:
 except StopIteration:
 	print('遍历完毕')
 
+# # 一、无限迭代器
 # 2、迭代器模块itertools
 # itertools包自带三个可以无限迭代的迭代器
-# ①count(初值=0，步长=1)
+print('①count(初值=0，步长=1)')
 from itertools import count
 for i in count(10,2):
     if i > 20:
@@ -19,12 +20,12 @@ for i in count(10,2):
     else:
         print(i)
 
-# ②islice(遍历的对象，迭代次数)
+print('②islice(遍历的对象，迭代次数)')
 from itertools import islice
 for i in islice(count(10),5):
     print(i)
 
-# ③cycle(可迭代对象)
+print('③cycle(可迭代对象)')
 from itertools import cycle
 count = 0
 for item in cycle('XYZ'):
@@ -32,3 +33,26 @@ for item in cycle('XYZ'):
         break
     print(item)
     count += 1
+
+print('④repeat(对象[，次数])')
+# repeat 迭代器会一遍遍地返回传入的对象直至永远，除非你设定了 times 参数。
+from itertools import repeat
+iterator = repeat(5,5)
+try:
+    for i in range(10):
+        print(iterator.__next__())
+except Exception as e:
+    print('迭代完毕')
+else:
+    pass
+finally:
+    pass
+
+# # 二、可终止的迭代器
+# 1、accumulate(可迭代对象[, 函数])
+# 2、chain.from_iterable(可迭代对象)
+# 3、compress(数据, 选择器)
+# 4、dropwhile(断言, 可迭代对象)
+# 5、filterfalse(断言, 可迭代对象)
+# 6、groupby(可迭代对象, 键=None)
+。。。。。。
